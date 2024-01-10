b2e41d3fc07b08803d8baa7a81c63fb6f2bac224
ducndh <ducndh@gmail.com>
2024-01-11 06:57:02 +0700

    half finish

---
+++ sum/sum.go
@@ -0,0 +1,38 @@
+package main
+
+func Sum(numbers []int) int {
+	var sum int
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, number := range numbersToSum {
+		sums = append(sums, Sum(number))
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, number := range numbersToSum {
+		if len(number) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := number[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
+
+func GetTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, number := range numbersToSum {
+		sums = append(sums, number[len(numbersToSum)-1])
+	}
+	return sums
+}
